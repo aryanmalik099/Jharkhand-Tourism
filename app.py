8ce5bc1585a28981137601b4225e8b75e88045e3
Mr Harsh <mrharshdrive@gmail.com>
2025-09-12 16:44:50 +0530

    third

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Pass4@ryan'
+app.config['MYSQL_PASSWORD'] = 'password'
 app.config['MYSQL_DB'] = 'jharkhand'
 
 mysql = MySQL(app)
