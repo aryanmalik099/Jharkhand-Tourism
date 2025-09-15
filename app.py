52c2ab0bfaaf3dd6a01c9d6c1600a9344f652e98
Mr-Harsh-Prasad <mrharshdrive@gmail.com>
2025-09-15 14:25:21 +0530

    before profile

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask_mysqldb import MySQL
 import os
 import re
 
-
 app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
@@ -14,59 +13,60 @@ app.config['MYSQL_DB'] = 'jharkhand'
 mysql = MySQL(app)
 app.secret_key = 'root'
 
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    msg=''
+
+@app.route('/auth', methods=['GET', 'POST'])
+def auth():
+    msg = ''
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        
-        cur = mysql.connection.cursor()
-        cur.execute("SELECT * FROM accounts WHERE username = %s AND password = %s", (username, password))
-        acc = cur.fetchone()
-        if acc:
-            session['loggedin'] = True
-            session['id'] = acc
-            session['username'] = acc[1]
-            return redirect(url_for('home'))
-        else:
-            msg = 'Invalid Credentials'
-    return render_template('login.html',msg=msg,page = 'login')
+        action = request.form.get('action')
 
+        if action == 'login':
+            username = request.form['login_username']
+            password = request.form['login_password']
 
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    msg = 'CREATE NEW ACCOUNT'
-    if request.method == 'POST':
-        username = request.form['username']
-        email = request.form['email']
-        password = request.form['password']
+            cur = mysql.connection.cursor()
+            cur.execute("SELECT * FROM accounts WHERE username = %s AND password = %s", (username, password))
+            acc = cur.fetchone()
 
-        cur = mysql.connection.cursor()
-        cur.execute("SELECT * FROM accounts WHERE username = %s", (username,))
-        acc = cur.fetchone()
-        if acc:
-            msg = 'Account already exists!'
-            return render_template('register.html', msg=msg)
-        elif re.match(r'[^@]+@[^@]+\.[^@]+', email) is None:
-            msg = 'Invalid email address!'
-            return render_template('register.html', msg=msg)
-        elif not re.match(r'[A-Za-z0-9]+', username):
-            msg = 'Username must contain only characters and numbers!'
-            return render_template('register.html', msg=msg)
-        elif not username or not password or not email:
-            msg = 'Please fill out the form!'
-            return render_template('register.html', msg=msg)
-        else:
-            cur.execute("INSERT INTO accounts (username, password, email) VALUES (%s, %s, %s)", (username, password, email))
-            cur.connection.commit()
-            msg = 'You have successfully registered!'
-            return render_template('login.html', msg=msg)
-    return render_template('register.html', msg=msg, page = 'register')
+            if acc:
+                session['loggedin'] = True
+                session['id'] = acc[0]
+                session['username'] = acc[1]
+                return redirect(url_for('home'))
+            else:
+                msg = 'Invalid Login Credentials'
+
+        elif action == 'register':
+            username = request.form['register_username']
+            email = request.form['register_email']
+            password = request.form['register_password']
+
+            cur = mysql.connection.cursor()
+            cur.execute("SELECT * FROM accounts WHERE username = %s", (username,))
+            acc = cur.fetchone()
+
+            if acc:
+                msg = 'Account already exists!'
+            elif re.match(r'[^@]+@[^@]+\.[^@]+', email) is None:
+                msg = 'Invalid email address!'
+            elif not re.match(r'[A-Za-z0-9]+', username):
+                msg = 'Username must contain only characters and numbers!'
+            elif not username or not password or not email:
+                msg = 'Please fill out the form!'
+            else:
+                cur.execute(
+                    "INSERT INTO accounts (username, password, email) VALUES (%s, %s, %s)",
+                    (username, password, email)
+                )
+                cur.connection.commit()
+                msg = 'You have successfully registered! You can now login.'
+
+    return render_template('auth.html', msg=msg, page='auth')
 
 
 @app.route('/logout')
@@ -81,18 +81,15 @@ def logout():
 def planner():
     return render_template('planner.html')
 
+
 @app.route('/gallery')
 def gallery():
     return render_template('gallery.html')
 
+
 @app.route('/destinations')
 def destinations():
-    return render_template('destinations.html')
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
-
+    return render_template('betla.html')
 
 
 if __name__ == '__main__':
